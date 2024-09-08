dd07b9452b2a3122b3da8fc4919e0c2a9d467216
Will Rogers <itsakettle@gmail.com>
2024-09-08 18:29:51 +0100

    start tables enum

--- data/grid_data/table_manager.py
+++ data/grid_data/table_manager.py
@@ -1,11 +1,7 @@
 import polars as pl
+from tables import Tables
 
-tables = [{"system_imbalance_price": {"period": pl.Utf8, 
-                      "imbalance_volume": pl.Float32, 
-                      "imbalance_price": pl.Float32, 
-                      "imbalance_cost": pl.Float32}}]
-
-def append(column_data: dict, to_delta_path: str, schema: dict):
+def append(column_data: dict, table: Tables):
     df = pl.DataFrame(data=column_data, schema=schema)
     df.write_delta(target=to_delta_path, mode="append")
 
